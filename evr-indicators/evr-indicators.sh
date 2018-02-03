96304f5b6e9bfaf8de6bf773acb84127903a09b9
x-ian <x-ian@x-ian.net>
2018-02-03 09:37:27 +0200

    sdf

--- evr-indicators/evr-indicators.sh
+++ evr-indicators/evr-indicators.sh
@@ -5,21 +5,27 @@
 # wait for random up to 60 seconds to prevent all systems hitting the network at the same time
 sleep $[ ( $RANDOM % 120 )  + 1 ]s
 
+CSV_VERSION=2
+
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 #HOST=192.168.22.22
-192.168.100.1
+HOST=192.168.100.1
 USER=user
 set timeout 10
-LOGIN="ssh $USER@$HOST"
+# for mikrotik bypass check of host key as this will change as J2s are moved around
+LOGIN="ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null $USER@$HOST"
 IP=`$LOGIN "ip addr pr" | grep TA-MTEMA | awk '{print $2}' | sed -r 's/.{3}$//'`
 NAME=`$LOGIN "sys ident print" | awk '{print $2}' | tr -d '\r\n'`
 TIMESTAMP=$(date +%Y%m%d-%H%M)
 MACS=$(cat /sys/class/net/*/address | tr "\n" " ")
 #SERIAL_NUMBER=`sudo dmidecode -t system  | grep Serial | awk '{print $3}'`
 SERIAL_NUMBER=`cat /home/user/evr-indicators/serial`
+DMI_MODEL=`dmesg | grep DMI | tr  ',' ' '`
 ROUTER_SERIAL=`$LOGIN "sys rout pri" | grep serial| awk '{print $2}' | tr -d '\r\n'`
 ROUTER_MODEL=`$LOGIN "sys rout pri" | grep model| awk '{print $2}' | tr -d '\r\n'`
-CSV_VERSION=1
+PARTITION_SIZE=`df -H /dev/sda1 | tail -1 |awk '{print $2}'`
+DISK_MODEL_VERBOSE=`udisksctl status | grep sda`
+DISK_MODEL="${DISK_VERBOSE:0:25}"
 
 ########## connectivity ##########
 
@@ -91,11 +97,11 @@ CRASH_COUNT=$(cat $SCRIPTPATH/evr-crashcount.log)
 
 mkdir -p $NAME
 
-# print header & values to screen
-echo '$CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT'
-echo $CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT
-echo '$CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT' > /home/user/evr-indicators/$NAME/indicators-header.txt 
-echo $CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT  >> /home/user/evr-indicators/$NAME/indicators.xls
+# print header & values to screen and to files
+echo '$CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE'
+echo $CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE
+echo '$CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE' > /home/user/evr-indicators/$NAME/indicators-header.txt 
+echo $CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE  >> /home/user/evr-indicators/$NAME/indicators.xls
 
 ## Tracking serial number of hardware per site
 echo $NAME,$TIMESTAMP, $SERIAL_NUMBER >> /home/user/evr-indicators/$NAME/serial-number.xls
@@ -104,7 +110,7 @@ echo $TIMESTAMP  $MESH_NB_SIGNAL_2G >> /home/user/evr-indicators/$NAME/neibor-si
 echo $TIMESTAMP  $MESH_NB_SIGNAL_5G >> /home/user/evr-indicators/$NAME/neibor-signa5.0G
 #echo $TIMESTAMP $SCAN_CLUSTERS >> /home/user/evr-indicators/$NAME/scan-clusters
 
-# wait for random up to 5 seconds to allow the data collection process to finalize before syncing the data with EVR server
+# wait for 5 seconds to allow the data collection process to finalize before syncing the data with EVR server
 sleep 5
 
 /usr/bin/rsync -arv --append /home/user/evr-indicators/$NAME user@192.168.21.254:~/evr-indicators
