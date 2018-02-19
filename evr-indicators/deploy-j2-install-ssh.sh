aee19b84e50dcbc9fa656963c557a74a428396ae
x-ian <x-ian@x-ian.net>
2018-02-19 14:21:36 +0200

    drin

---
+++ evr-indicators/deploy-j2-install-ssh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LOGIN_J2=user@192.168.85.194
+
+ssh $LOGIN_J2 <<EOF
+mkdir evr-indicators
+mkdir evr-indicators/ssh-keys-from-evr-server
+EOF
+
+scp install.sh evr-bootcount.sh evr-indicators.sh get-serial.sh maintenance.sh update-scripts-from-server.sh $LOGIN_J2:evr-indicators/
+scp ssh-keys-from-evr-server/* $LOGIN_J2:evr-indicators/ssh-keys-from-evr-server
+
+ssh $LOGIN_J2 <<EOF
+cd evr-indicators
+./install.sh
+EOF
