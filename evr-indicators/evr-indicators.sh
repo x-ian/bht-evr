113ebe9e345bcd09cdd199275d538108ee5322d2
x-ian <x-ian@x-ian.net>
2018-07-18 14:42:25 +0200

    v5

--- evr-indicators/evr-indicators.sh
+++ evr-indicators/evr-indicators.sh
@@ -10,9 +10,9 @@ sleep $[ ( $RANDOM % 120 )  + 1 ]s
 # run sudo visudo and add this line: user ALL=(ALL) NOPASSWD: /bin/date
 #SERVER_TIME=`ssh user@192.168.21.254 date`
 #sudo date --set="$SERVER_TIME"
-SERVER_TIME=`ssh user@192.168.21.254 date +%Y%m%d-%H%M`
+SERVER_TIME=`ssh -o PasswordAuthentication=no user@192.168.21.254 date +%Y%m%d-%H%M`
 
-CSV_VERSION=4
+CSV_VERSION=5
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 #HOST=192.168.22.22
@@ -38,6 +38,7 @@ DMI_MODEL_VERBOSE=`dmesg | grep DMI | head -1 | tr  ',' ' '`
 DMI_MODEL="${DMI_MODEL_VERBOSE:38:80}"
 ROUTER_SERIAL=`$LOGIN "sys rout pri" | grep serial| awk '{print $2}' | tr -d '\r\n'`
 ROUTER_MODEL=`$LOGIN "sys rout pri" | grep model| awk '{print $2}' | tr -d '\r\n'`
+ROUTER_MAC=`$LOGIN "interface wireless print" | grep Main | awk '{print $5}'`
 PARTITION_SIZE=`df -H | tail -1 |awk '{print $2}'`
 DISK_MODEL_VERBOSE=`udisksctl status | grep sda`
 DISK_MODEL="${DISK_MODEL_VERBOSE:0:25}"
@@ -50,13 +51,38 @@ MT_CPU_LOAD=`$LOGIN "sys resource print" | grep cpu-load | awk '{print $2}' | tr
 
 ########## connectivity ##########
 
+MESH_CLUSTER=`$LOGIN "interface wireless print" | grep " ssid=" | awk '{print $4}' | tr -d '\r\n' | sed -r 's/ssid=/ /g'`
+
 # mesh number of neigbhors
 MESH_NEIGHBOR_COUNT=`$LOGIN "int wir reg pri" | grep -c Main` 
+MESH_NEIGHBOR_COUNT_2G=`$LOGIN "int wir reg pri" | grep -c 2.4G` 
 
 #MESH_NB_SIGNAL_5G=`$LOGIN "int wir reg pri" | grep Main| awk '{print $3 "\t"  $6}' | sed -r 's/.{9}$//'`
 MESH_NB_SIGNAL_5G=`$LOGIN "int wir reg pri" | grep Main| awk '{print $3 "\t"  $6}'`
 MESH_NB_SIGNAL_2G=`$LOGIN "int wir reg pri" | grep 2.4G| awk '{print $3 "\t"  $6}'`
 #SCAN_CLUSTERS=`$LOGIN  "interface wireless scan Main duration=2" | grep   evr | awk '{print $4, $8, $11}'`
+
+# attempt to get the RSSPT ports and neighbours right
+# interface print
+# interface wireless print
+# interface wireless wds print
+# interface bridge port print
+# interface bridge port monitor X
+# interface bridge monitor X
+# {
+# :local stationName;
+# :local stationMAC;
+# :local stationHost;
+# :local stationInterface;
+# :foreach i in=[/interface wireless registration-table find] do={
+#  :set $stationName [/interface wireless registration-table get $i radio-name];
+#  :set $stationMAC [/interface wireless registration-table get $i mac-address];
+#  :set $stationHost [/interface bridge host get [find where mac-address=$stationMAC]];
+#  :set $stationInterface ($stationHost->"on-interface");
+#  :put "Station \"$stationName\" ($stationMAC) is connected by \"$stationInterface\" interface";
+# }
+# }
+
 
 ########## ping ##########
 
@@ -125,10 +151,10 @@ CRASH_COUNT=$(cat $SCRIPTPATH/evr-crashcount.log)
 mkdir -p $NAME
 
 # print header & values to screen and to files
-echo '$CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE,$LINUX_VERSION,$UPTIME,$MT_VOLTAGE,$MT_TEMP,$MT_UPTIME,$MT_CPU_LOAD,$SERVER_TIME'
-echo $CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE,$LINUX_VERSION,$UPTIME,$MT_VOLTAGE,$MT_TEMP,$MT_UPTIME,$MT_CPU_LOAD,$SERVER_TIME
-echo '$CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE,$LINUX_VERSION,$UPTIME,$MT_VOLTAGE,$MT_TEMP,$MT_UPTIME,$MT_CPU_LOAD,$SERVER_TIME' > ~/evr-indicators/$NAME/indicators-header.txt 
-echo $CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE,$LINUX_VERSION,$UPTIME,$MT_VOLTAGE,$MT_TEMP,$MT_UPTIME,$MT_CPU_LOAD,$SERVER_TIME  >> ~/evr-indicators/$NAME/indicators.xls
+echo '$CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE,$LINUX_VERSION,$UPTIME,$MT_VOLTAGE,$MT_TEMP,$MT_UPTIME,$MT_CPU_LOAD,$SERVER_TIME,$ROUTER_MAC,$MESH_NEIGHBOR_COUNT_2G,$MESH_CLUSTER'
+echo $CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE,$LINUX_VERSION,$UPTIME,$MT_VOLTAGE,$MT_TEMP,$MT_UPTIME,$MT_CPU_LOAD,$SERVER_TIME,$ROUTER_MAC,$MESH_NEIGHBOR_COUNT_2G,$MESH_CLUSTER
+echo '$CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE,$LINUX_VERSION,$UPTIME,$MT_VOLTAGE,$MT_TEMP,$MT_UPTIME,$MT_CPU_LOAD,$SERVER_TIME,$ROUTER_MAC,$MESH_NEIGHBOR_COUNT_2G,$MESH_CLUSTER' > ~/evr-indicators/$NAME/indicators-header.txt 
+echo $CSV_VERSION,$IP,$NAME,$TIMESTAMP,$MACS,$SERIAL_NUMBER,$ROUTER_SERIAL,$ROUTER_MODEL,$PING_EXIT_CODE,$PING_DUPS,$PING_PACKET_LOSS,$PING_RRT_AVG,$HTTP_SPEED,$MESH_NEIGHBOR_COUNT,$STARTUP_COUNT,$CRASH_COUNT,$DMI_MODEL,$DISK_MODEL,$PARTITION_SIZE,$LINUX_VERSION,$UPTIME,$MT_VOLTAGE,$MT_TEMP,$MT_UPTIME,$MT_CPU_LOAD,$SERVER_TIME,$ROUTER_MAC,$MESH_NEIGHBOR_COUNT_2G,$MESH_CLUSTER  >> ~/evr-indicators/$NAME/indicators.xls
 
 ## Tracking serial number of hardware per site
 echo $NAME,$TIMESTAMP, $SERIAL_NUMBER >> ~/evr-indicators/$NAME/serial-number.xls
