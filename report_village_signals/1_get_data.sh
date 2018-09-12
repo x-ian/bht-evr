3877e9760458c842e6daf837722428f568135563
Christian Neumann <x-ian@x-ian.net>
2018-09-12 21:01:44 +0200

    newer

--- report_village_signals/1_get_data.sh
+++ report_village_signals/1_get_data.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-rm /tmp/village_signals.csv 
+MYSQL_TABLE=evr_indicators_v8
+MYSQL_USER="evr"
+MYSQL_PW="evr"
+MYSQL_DB="evr_monitoring"
+
+rm -rf /tmp/village_signals
+mkdir /tmp/village_signals
+chmod a+w /tmp/village_signals
+
+ALL_SITES=`mysql -u $MYSQL_USER -p$MYSQL_PW $MYSQL_DB --skip-column-names -e "select distinct(upper(name)) from $MYSQL_TABLE" -B`
+
 
 # traffic each minute for last 60 minutes
 mysql -u root -pxian evr --skip-column-names <<EOF
