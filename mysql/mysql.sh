933374d381987653bff14e520cc13883b18e9c5b
Christian Neumann <x-ian@x-ian.net>
2018-09-21 20:57:00 +0200

    ..

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /home/user/evr-indicators
-find . -name indicators.xls -exec ./mysql_import.sh {} \;
+find . -name indicators.xls -exec /home/user/bht-evr/mysql/mysql_import.sh {} \;
 
-./mysql_import_post_process.sh
+/home/user/bht-evr/mysql/mysql_import_post_process.sh
 
-./mysql_report.sh
+/home/user/bht-evr/mysql/mysql_report.sh
 
