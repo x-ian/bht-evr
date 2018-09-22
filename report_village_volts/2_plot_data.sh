bea099b6d0a292b87f48564434e707102f6d573a
Christian Neumann <x-ian@x-ian.net>
2018-09-22 09:33:41 +0200

    sdf

--- report_village_volts/2_plot_data.sh
+++ report_village_volts/2_plot_data.sh
@@ -5,7 +5,7 @@ ALL=`ls -1 /tmp/village_volts`
 while read -r line; do
     echo $line
     gnuplot <<EOF
-call "/home/user/evr-indicators/report_village_volts/plot_data.gp" "($line)" "/tmp/village_volts/$line" "/var/www/html/evr-village-volts/$line.png"
+call "/home/user/bht-evr/report_village_volts/plot_data.gp" "($line)" "/tmp/village_volts/$line" "/var/www/html/evr-village-volts/$line.png"
 EOF
 done <<< "$ALL"
 
